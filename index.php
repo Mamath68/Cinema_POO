0ab58b8bb7542cb25f87d880dac1db5e9777e8b9
Mamath68 <mathieu.stamm@gmail.Com>
2023-03-30 14:37:43 +0200

    architecture

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
-<title>Cinema_POO</title>
+<!DOCTYPE html>
+<html lang="fr">
 
-<?php
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cinema_POO</title>
+</head>
 
-include "Acteur.php";
-include "Realisateur.php";
+<body>
+    <?php
 
-?>
+    spl_autoload_register(function ($class_name) {
+        include $class_name . '.php';
+    });
+
+    ?>
+</body>
+
+</html>
