72e8f4aaa08ca9425aa4320499f876e8f32e4de3
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2023-07-31 15:42:42 +0200

    Update listFilms.php

--- app/view/film/listFilms.php
+++ app/view/film/listFilms.php
@@ -1,29 +1,35 @@
 <?php ob_start()
-  ?>
+?>
 
-<h1>Bienvenue sur ma page de Film</h1><br>
+<h1>Bienvenue sur ma page de Film</h1> <br>
 <h2>Vous avez <?=$films->rowCount()?> films</h2> 
-<?php
-echo '<table class="table">
-    <thead>
-      <tr>
+
+<table class="table">
+  <thead>
+    <tr>
       <th scope="col">Titre</th>
       <th scope="col">Affiche</th>
       <th scope="col">Details</th>
       <th scope="col">Supprimer</th>
-      </tr>
-    </thead>';
-while ($film = $films->fetch()) {
-  echo '<tbody>
+    </tr>
+  </thead>
+  <?php
+  while ($film = $films->fetch()) {
+    ?>
+    <tbody>
       <tr>
-      <td>' . $film['titre'] . '</td>
-      <td><img src="' . $film['img'] . '" class="img-fluid"></td>
-      <td><a href ="index.php?action=detailFilm&id=' . $film['id_film'] . '">Details</a></td>
-      <td><a href ="index.php?action=supprimerFilm&id=' . $film['id_film'] . '">Supprimer</a></td>
-      </tr>';
-}
-echo ' </tbody>
-      </table>';
+        <td>
+          <?= $film['titre'] ?>
+        </td>
+        <td><img src="<?= $film['img'] ?>" class="img-fluid"></td>
+        <td><a href="index.php?action=detailFilm&id=<?= $film['id_film'] ?>">Details</a></td>
+        <td><a href="index.php?action=supprimerFilm&id=<?= $film['id_film'] ?>">Supprimer</a></td>
+      </tr>
+      <?php
+  }
+  ?>
+  </tbody>
+</table>
 ?>
 <a href="#" class="go_top">
   <i class="fa-solid fa-arrow-up"></i>
