a11d137a6db845a9aadfd2425adb09ce26750265
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2023-07-31 15:46:19 +0200

    Update detailRealisateur.php

--- app/view/realisateur/detailRealisateur.php
+++ app/view/realisateur/detailRealisateur.php
@@ -1,31 +1,39 @@
 <?php ob_start();
 $realisateur = $realisateur->fetch();
 ?>
-<?php echo '<h1>' . $realisateur['nom_realisateur'] . '</h1>' ?>
+<h1>
+  <?= $realisateur['nom_realisateur'] ?>
+</h1>
 
-<?php
-echo
-  '<div class="container text-center">
+<div class="container text-center">
   <div class="row">
-    <div class="col"><img src="' . $realisateur['img'] . '" class="img-fluid"></div>
+    <div class="col"><img src="<?= $realisateur['img'] ?>" class="img-fluid"></div>
     <div class="col">
-    <div>' . 'Date de naissance : ' . ' ' . $realisateur['date_naissance'] . '</div>
-    <div>' . 'Nationalité : ' . '' . $realisateur['nationalite'] . '</div>
+      <div>Date de naissance :
+        <?= $realisateur['date_naissance'] ?>
+      </div>
+      <div>Nationalité :
+        <?= $realisateur['nationalite'] ?>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <section>
+        <div>
+          <h2>Liste du/des Films</h2>
+        </div>
+        <?php
+        foreach ($film as $film) {
+          ?>
+          <div><a href="index.php?action=detailFilm&id=<?= $film['id_film'] ?>"><?= $film['titre'] ?></a></div>
+          <?php
+        }
+        ?>
+      </section>
+    </div>
   </div>
 </div>
-<div class="row">
-  <div class="col">
-    <section>
-    <div><h2>Liste du/des Films</h2></div>';
-foreach ($film as $film) {
-  echo '<div><a href ="index.php?action=detailFilm&id=' . $film['id_film'] . '">' . $film['titre'] . '</a></div>';
-}
-echo
-  '</section>
-  </div>
-</div>
-</div>';
-?>
 
 <?php
 $title = $realisateur['nom_realisateur'];
