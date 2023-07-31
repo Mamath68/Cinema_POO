57a046fba4685ec43caa4f1954db6bad90519381
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2023-07-31 15:45:57 +0200

    Update listRealisateurs.php

--- app/view/realisateur/listRealisateurs.php
+++ app/view/realisateur/listRealisateurs.php
@@ -3,30 +3,34 @@
 
 <h1>Bienvenue sur ma page des realisateurs</h1>
 
-<?php
-
-echo '<table class="table">
-<thead>
-  <tr>
-    <th colspan="2">Prenom Nom</th>
-    <th scope="col">Images</th>
-    <th scope="col">Details</th>
-  </tr>
-</thead>';
-while ($realisateur = $realisateurs->fetch()) {
-  echo '<tbody>
+<table class="table">
+  <thead>
     <tr>
-      <td colspan="2">' . $realisateur['nom_realisateur'] . '</td>
-      <td><img src="' . $realisateur['img'] . '" class="img-fluid"></td>
-      <td><a href ="index.php?action=detailRealisateur&id=' . $realisateur['id_realisateur'] . '">Details</a></td>
-    </tr>';
-}
-echo '</tbody>
-</table>';
-?>
- <a href="#" class="go_top">
-        <i class="fa-solid fa-arrow-up"></i>
-    </a>
+      <th colspan="2">Prenom Nom</th>
+      <th scope="col">Images</th>
+      <th scope="col">Details</th>
+    </tr>
+  </thead>
+  <?php
+  while ($realisateur = $realisateurs->fetch()) {
+    ?>
+    <tbody>
+      <tr>
+        <td colspan="2">
+          <?= $realisateur['nom_realisateur'] ?>
+        </td>
+        <td><img src="<?= $realisateur['img'] ?>" class="img-fluid"></td>
+        <td><a href="index.php?action=detailRealisateur&id=<?= $realisateur['id_realisateur'] ?>">Details</a></td>
+      </tr>
+      <?php
+  }
+  ?>
+  </tbody>
+</table>
+
+<a href="#" class="go_top">
+  <i class="fa-solid fa-arrow-up"></i>
+</a>
 <?php
 
 $title = "Liste Des Réalisateur";
