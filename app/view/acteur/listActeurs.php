de87f903f2df616c898ae4857ff0f738f5562d47
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2023-07-31 15:44:03 +0200

    Update listActeurs.php

--- app/view/acteur/listActeurs.php
+++ app/view/acteur/listActeurs.php
@@ -3,30 +3,35 @@
 
 <h1>Bienvenue sur ma page des acteurs</h1>
 
-<?php
 
-echo '<table class="table">
-<thead>
-  <tr>
-    <th colspan="2">Prenom Nom</th>
-    <th scope="col">Image</th>
-    <th scope="col">Details</th>
-  </tr>
-</thead>';
-while ($acteur = $acteurs->fetch()) {
-  echo '<tbody>
+<table class="table">
+  <thead>
+    <tr>
+      <th colspan="2">Prenom Nom</th>
+      <th scope="col">Image</th>
+      <th scope="col">Details</th>
+    </tr>
+  </thead>
+  <?php
+  while ($acteur = $acteurs->fetch()) {
+    ?>
+    <tbody>
       <tr>
-        <td colspan="2">' . $acteur['nom_acteur'] . '</td>
-        <td><img src="' . $acteur['img'] . '" class="img-fluid"></td>
-        <td><a href ="index.php?action=detailActeur&id=' . $acteur['id_acteur'] . '">Details</a></td>
-      </tr>';
-}
-echo '</tbody>
-  </table>';
-?>
- <a href="#" class="go_top">
-        <i class="fa-solid fa-arrow-up"></i>
-    </a>
+        <td colspan="2">
+          <?= $acteur['nom_acteur'] ?>
+        </td>
+        <td><img src="<?= $acteur['img'] ?>" class="img-fluid"></td>
+        <td><a href="index.php?action=detailActeur&id=<?= $acteur['id_acteur'] ?>">Details</a></td>
+      </tr>
+      <?php
+  }
+  ?>
+  </tbody>
+</table>
+
+<a href="#" class="go_top">
+  <i class="fa-solid fa-arrow-up"></i>
+</a>
 <?php
 
 $title = "Liste Des Acteurs";
